e93fab0bec035ad2abb19ad254f4f38d0b32b77b
aperfect01 <andyperezqp2@gmail.com>
2026-02-03 01:47:10 +0800

    Updates

---
+++ assets/Services-M8JzlTGL.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BAvMnzz3.js";import{C as t}from"./Container-BQ_ftQO5.js";const o=()=>e.jsx(t,{children:e.jsx("h1",{className:"text-4xl font-bold mb-4 text-center",children:"Services"})});export{o as default};
