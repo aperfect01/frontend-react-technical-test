e111851ca396945412dd06c499a7e6a6a68dcbce
aperfect01 <andyperezqp2@gmail.com>
2026-02-02 23:27:04 +0800

    feat: refactor Header component to use NavLink for navigation and improve styling

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { navigationLinks } from "../../data/navigation";
 import { Button } from "../common/Button";
 
@@ -5,16 +6,20 @@ export const Header = () => {
   return (
     <header className="bg-transparent py-10">
       <div className="flex justify-between items-center px-4">
-        <h1 className="text-3xl font-bold text-gray-800">My Website</h1>
-        <div className="space-x-4">
+        <h1 className="text-3xl font-bold">My Website</h1>
+        <div className="space-x-20">
           {navigationLinks.map((link) => (
-            <a
+            <NavLink
               key={link.name}
-              href={link.path}
-              className="text-gray-600 hover:text-gray-800 font-medium"
+              to={link.path}
+              className={({ isActive }) =>
+                `font-bold transition-colors ${
+                  isActive ? "text-primary" : "text-black hover:text-primary"
+                }`
+              }
             >
               {link.name}
-            </a>
+            </NavLink>
           ))}
         </div>
         <Button label="Get Started" onClick={() => alert("Button Clicked!")} />
